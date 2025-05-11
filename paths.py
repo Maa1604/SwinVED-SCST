2240ee3746d29eca7542e5caef211c4c7ca6d450
Maa1604 <mohamedaasalas@gmail.com>
2025-05-11 23:44:24 +0200

    paths modificados

--- paths.py
+++ paths.py
@@ -1,12 +1,12 @@
 IMAGES_MIMIC_PATH = "/home/Data/NEW/mimic-cxr/2.0.0/files_jpg_512/files"
 
 DICT_CSV_MIMIC_PATH = {
-    "train": "/home/maasala/RadiologyVQA/LlamaDATA/generated_questions_answers_test_all_peque.csv",
-    "validation": "/home/maasala/RadiologyVQA/LlamaDATA/generated_questions_answers_validate_all.csv",
-    "test": "/home/maasala/RadiologyVQA/LlamaDATA/generated_questions_answers_validate_all_peque.csv"
+    "train": "/home/maasala/llama-vqa/LlamaDATA/generated_questions_answers_test_all_peque.csv",
+    "validation": "/home/maasala/llama-vqa/LlamaDATA/generated_questions_answers_validate_all.csv",
+    "test": "/home/maasala/llama-vqa/LlamaDATA/generated_questions_answers_validate_all_peque.csv"
 }
 
-VOCAB_PATH = "/home/maasala/RadiologyVQA/LlamaDATA/vocab.tgt"
+VOCAB_PATH = "/home/maasala/llama-vqa/LlamaDATA/vocab.tgt"
 
 SWINB_IMAGENET22K_WEIGHTS = "microsoft/swin-base-patch4-window12-384-in22k"
 SWINB_IMAGENET22K_WEIGHTS_FINETUNE = "/home/maasala/RadiologyVQA/swin_mimic"
