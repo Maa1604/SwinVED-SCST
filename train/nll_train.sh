59712b10affc1cdc46455ed45a5ed841e3778263
Maa1604 <mohamedaasalas@gmail.com>
2025-05-28 00:27:12 +0200

    Quitamos archivos inecesarios de esta rama

--- train/nll_train.sh
+++
@@ -1,8 +0,0 @@
-# conda activate hroc
-
-python mytrain_nll.py \
-    --exp_name SwinBERT_UnfreezedEncoder \
-    --model_arch SwinBERTFinetuned \
-    --hnm True \
-    --load_weights "SwinBERT_FreezedEncoder/best_bleu1_6_model.pt"
-    # --metrics_on_train
